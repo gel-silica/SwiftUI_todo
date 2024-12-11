ce6121c8616d15650baf525008371ca50c8fdf70
silicagel <ksk19981004@gmail.com>
2024-12-12 06:50:16 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by silicagel on 2024/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
